e213480c3a3f1ebc71755ae6954d12b8d53d37d1
Chris Kaltwasser <chris@chrisk.com>
2021-12-11 17:05:59 -0500

    two ports work

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Opt {
     input_port: u32,
     #[structopt(default_value = DEFAULT_RESPONSE_PORT, short = "o", long = "output-port")]
     output_port: u32,
-    #[structopt(default_value = "localhost", short = "h", long = "host")]
+    #[structopt(default_value = "localhost", short = "s", long = "host")]
     host: String,
     #[structopt(short = "r", long = "request")]
     request_file: Option<String>,
@@ -36,14 +36,13 @@ fn handle_client(mut request_stream: TcpStream, opt: Opt)
         } else {
         r#"
 {
-  "command": "RSHOT",
-  "objectType": "eventJSON",
-  "siteCode": "ds12", 
-  "holeNumber": "hole3",  
-  "archiveFilename": "Archive_211001_140321", 
-  "archivePath": "./path/to/archive/file/"
+   "command" : "FETCH",
+   "parameters" : [
+      "REPLAY",
+      "archive_211015_123637"
+   ]
 }
-"#.to_string()
+"#.trim().to_string()
         };
 
     let rq_json = serde_json::from_str::<Value>(&sample_request[..]).unwrap();
@@ -58,16 +57,21 @@ fn handle_client(mut request_stream: TcpStream, opt: Opt)
     // Note we send the original request that has been validated
     // by this point in the code.
 
-    let len_msg = format!("{:08x}", sample_request.len());
+    let req_len = sample_request.len();
+    let len_msg = format!("{:08x}", req_len);
+    println!("TP001: req-strm write <length message>: msg={} len={}",
+        len_msg, req_len);
     let wresult = request_stream.write(len_msg.as_bytes());
 
     match wresult {
         Err(e) => {
-            println!("error writing len_msg: {}", e);
+            println!("error writing len_msg: e={}", e);
         }
         _ => {}
     }
 
+    println!("TP002: req-stm write <sample message>: msg={} len={}",
+                sample_request, sample_request.len());
     let wresult = request_stream.write(sample_request.as_bytes());
     match wresult {
         Err(e) => {
@@ -75,7 +79,9 @@ fn handle_client(mut request_stream: TcpStream, opt: Opt)
         }
         _ => {}
     }
+    println!("TP002.1: write returned");
 
+    println!("TP003: resp-strm connect");
     let mut response_stream =
         match TcpStream::connect(get_url(&opt.host, opt.output_port)) {
             Ok(response) => response,
@@ -84,37 +90,47 @@ fn handle_client(mut request_stream: TcpStream, opt: Opt)
                 std::process::exit(1);
             }
         };
-
+    println!("TP003.1: connected to response_stream");
     let mut read_count = 100;
 
+    let mut tp_i = 4;
     loop {
+        // read len msg
         let mut len_buf: [u8; 8] = [0; 8];
+        println!("TP004: read_exact called for {} bytes from response stream",
+            len_but.len());
         response_stream.read_exact(&mut len_buf).unwrap();
-        println!("8 bytes read = {:?}", len_buf);
+        println!("TP00{}.1: 8 bytes read = {:?}", tp_i, len_buf);
         let len_str = str::from_utf8(&len_buf).unwrap();
         let bytes_to_read: usize
             = usize::from_str_radix(len_str.trim(), 16).unwrap();
-        println!("converts to hex str={} or bytes_to_read={}", len_str, bytes_to_read);
+        println!("TP00{}.2: converts to hex str={} or bytes_to_read={}",
+            tp_i, len_str, bytes_to_read);
+
+        tp_i += 1;
 
         // read exactly `bytes_to_read` len and error if not 
         // valid json
         let mut response_buf = vec![0u8; bytes_to_read];
+        println!("TP00{}: read_exact len={}", tp_i, response_buf.len());
         response_stream.read_exact(&mut response_buf).unwrap();
         let response = str::from_utf8(&response_buf).unwrap();
-        println!("{} bytes received=[{}]", bytes_to_read, response);
+        println!("TP00{}.1: bytes received=[{}]", tp_i, bytes_to_read, response);
 
         let rsp_json =
             serde_json::from_str::<Value>(response).unwrap();
 
         if opt.pretty_json {
             let rsp_json_text = serde_json::to_string_pretty(&rsp_json).unwrap();
-            println!("response(pretty validated)={}", rsp_json_text);
+            println!("TP00{}.2: response(pretty validated)={}", tp_i, rsp_json_text);
         }
 
         read_count -= 1;
         if read_count == 0 {
             break;
         }
+
+        tp_i += 1;
     }
 
     Ok(())
@@ -125,6 +141,7 @@ fn main() -> std::io::Result<()> {
     println!("opt={:?}", opt);
 
     let url = get_url(&opt.host, opt.input_port);
+    println!("TP000: connecting to req-strm");
     let request_stream =
         match TcpStream::connect(&url[..]) {
             Ok(stream) => stream,
